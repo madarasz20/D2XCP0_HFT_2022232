115790556a1655b4f2a3b8fc668771e966623d25
User <User@DESKTOP-L2CDRCS>
2023-10-18 15:03:58 +0200

    Add project files.

---
+++ D2XCP0_HFT_2022232.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace D2XCP0_HFT_2022232.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ D2XCP0_HFT_2022232.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace D2XCP0_HFT_2022232.Test
+{
+    public class Class1
+    {
+    }
+}

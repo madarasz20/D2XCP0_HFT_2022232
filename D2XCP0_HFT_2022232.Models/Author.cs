4b5e06080a8715fe19fb991b0f3edb239e48dbb1
madarasz.oli <madaraszoliver@gmail.com>
2023-10-25 08:28:35 +0200

    Author finished

--- D2XCP0_HFT_2022232.Models/Author.cs
+++ D2XCP0_HFT_2022232.Models/Author.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace D2XCP0_HFT_2022232.Models
 {
     public class Author
     {
+        [Key]
+        public int AuthorID { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string AuthorName { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        //Navigational property
+        public ICollection<Book> Books { get; set; }
     }
 }

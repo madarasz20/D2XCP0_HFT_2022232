09bf757727bfc09644acadc97ea3cb22206c97b9
madarasz.oli <madaraszoliver@gmail.com>
2023-10-25 08:34:44 +0200

    FK set

--- D2XCP0_HFT_2022232.Models/Book.cs
+++ D2XCP0_HFT_2022232.Models/Book.cs
@@ -15,18 +15,21 @@ namespace D2XCP0_HFT_2022232.Models
         [Required]
         [StringLength(70)]
         public string Title { get; set; }
+
+
+        [ForeignKey("AuthorID")]
         public int AuthorID { get; set; }
+        [ForeignKey("GenreID")]
         public int GenreID { get; set; }
+
 
         public int Price { get; set; }
         public double Rating { get; set; }
         public int NumberOfReviews { get; set; }
 
         // Navigation properties to represent the relationships
-        [ForeignKey("AuthorID")]
+        
         public virtual Author Author { get; set; }
-
-        [ForeignKey("GenreID")]
         public virtual Genre Genre { get; set; }
 
     }
--- D2XCP0_HFT_2022232.Models/Genre.cs
+++ D2XCP0_HFT_2022232.Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace D2XCP0_HFT_2022232.Models
 {
     public class Genre
     {
+        [Key]
+        public int GenreID { get; set; }
+        [Required]
+        public string GenreName { get; set; }
+
+        //Navigational property
+        public virtual ICollection<Book> Books { get; set; }
     }
 }

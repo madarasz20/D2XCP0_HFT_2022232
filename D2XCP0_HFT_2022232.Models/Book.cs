a772f7debc90ee3e839d0c6732e9f867437e5ab5
madarasz.oli <madaraszoliver@gmail.com>
2023-10-25 08:17:14 +0200

    Book constraints set

--- D2XCP0_HFT_2022232.Models/Book.cs
+++ D2XCP0_HFT_2022232.Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,18 @@ namespace D2XCP0_HFT_2022232.Models
 {
     public class Book
     {
+
+        [Key] 
         public int BookID { get; set; }
+        [Required]
+        [StringLength(70)]
         public string Title { get; set; }
         public int AuthorID { get; set; }
         public int GenreID { get; set; }
+
+        public int Price { get; set; }
+        public double Rating { get; set; }
+        public int NumberOfReviews { get; set; }
+
     }
 }

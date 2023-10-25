a74dc7a162b07f23cc9c0605049b29e89eda940e
madarasz.oli <madaraszoliver@gmail.com>
2023-10-25 10:52:51 +0200

    IRepository created

---
+++ D2XCP0_HFT_2022232.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using D2XCP0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2XCP0_HFT_2022232.Repository
+{
+    public interface IRepository
+    {
+        void Create(Book book);
+        Book Read(int id);
+        IQueryable<Book> ReadAll();
+        void Update(Book book);
+        void Delete(int id);
+    }
+}

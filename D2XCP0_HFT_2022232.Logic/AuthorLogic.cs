77ce5608d402795e364024d72a2cdd4e6fb8462f
madarasz.oli <madaraszoliver@gmail.com>
2023-11-13 16:59:41 +0100

    Authorlogic + IauthorLogic finished

---
+++ D2XCP0_HFT_2022232.Logic/AuthorLogic.cs
@@ -0,0 +1,49 @@
+﻿using D2XCP0_HFT_2022232.Models;
+using D2XCP0_HFT_2022232.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2XCP0_HFT_2022232.Logic
+{
+    public class AuthorLogic : IAuthorLogic
+    {
+        IRepository<Author> repo;
+
+        public AuthorLogic(IRepository<Author> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Author item)
+        {
+            if (item.AuthorName.Length > 0)
+                repo.Create(item);
+            else
+                throw new ArgumentException("Invalid author name");
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Author Read(int id)
+        {
+            return repo.Read(id);
+        }
+
+        public IEnumerable<Author> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Author item)
+        {
+            repo.Update(item);
+        }
+    }
+}
---
+++ D2XCP0_HFT_2022232.Logic/IAuthorLogic.cs
@@ -0,0 +1,19 @@
+﻿using D2XCP0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2XCP0_HFT_2022232.Logic
+{
+    public interface IAuthorLogic
+    {
+        void Create(Author item);
+        void Delete(int id);
+        Author Read(int id);
+        IEnumerable<Author> ReadAll();
+        void Update(Author item);
+    }
+}

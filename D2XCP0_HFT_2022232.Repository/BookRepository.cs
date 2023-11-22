fc66bcfa2072e867f2f4bde1e3babd1ecb0937cc
madarasz.oli <madaraszoliver@gmail.com>
2023-11-22 22:12:25 +0100

    modelrepos added

---
+++ D2XCP0_HFT_2022232.Repository/BookRepository.cs
@@ -0,0 +1,34 @@
+﻿using D2XCP0_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D2XCP0_HFT_2022232.Repository
+{
+    public class BookRepository : Repository<Book>, IRepository<Book>
+    {
+        public BookRepository(BookDbcontext ctx) : base(ctx)
+        {
+        }
+
+        public override Book Read(int id)
+        {
+            return ctx.Books.FirstOrDefault(t => t.BookID == id);
+        }
+
+        public override void Update(Book item)
+        {
+
+            var oldBook = Read(item.BookID);
+            oldBook.Title = item.Title;
+            oldBook.AuthorID = item.AuthorID;
+            oldBook.GenreID = item.GenreID;
+            oldBook.Price = item.Price;
+            oldBook.Rating = item.Rating;
+            oldBook.Pages = item.Pages;
+            this.ctx.SaveChanges();
+        }
+    }
+}

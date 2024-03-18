22ed0cbf40303bd5119da66b956031022574da6c
madarasz.oli <madaraszoliver@gmail.com>
2024-03-18 12:30:22 +0100

    Authors name binded

--- WpfClient/MainWindowViewModel.cs
+++ WpfClient/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using D2XCP0_HFT_2022232.Models;
+using MovieDbApp.WpfClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace WpfClient
 {
     public class MainWindowViewModel
     {
+        public RestCollection<Author> Authors { get; set; }
+        public MainWindowViewModel()
+        {
+            Authors = new RestCollection<Author>("http://localhost:20300/", "author");
+        }
     }
 }

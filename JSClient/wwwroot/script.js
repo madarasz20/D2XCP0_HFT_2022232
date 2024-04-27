6e1add9fea853c3af474f3571205fff661655326
madarasz.oli <madaraszoliver@gmail.com>
2024-04-27 18:19:42 +0200

    author js noncrud done

--- JSClient/wwwroot/script.js
+++ JSClient/wwwroot/script.js
@@ -1,13 +1,50 @@
 ﻿let authors = [];
+let connection = null;
+
+let authorIDtoUpdate = -1;
 
 getdata();
+setupSignalR();
+
+
+function setupSignalR() {
+    connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:20300/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+
+    connection.on("AuthorCreated", (user, message) => {
+        getdata();
+    });
+    connection.on("AuthorDeleted", (user, message) => {
+        getdata();
+    });
+    connection.on("AuthorUpdated", (user, message) => {
+        getdata();
+    });
+    connection.onclose(async () => {
+            await start();
+        });
+        start();
+
+}
+
+async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+}
 
 async function getdata() {
     await fetch('http://localhost:20300/author')
         .then(x => x.json())
         .then(y => {
             authors = y;
-            console.log(authors);
+            //console.log(authors);
             display();
         });
 
@@ -23,7 +60,8 @@ function display() {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.authorID + "</td><td>" + t.authorName + 
         "</td><td>" +
-        `<button type = "button" onclick = "remove(${t.authorID})">Delete</button>`
+        `<button type = "button" onclick = "remove(${t.authorID})">Delete</button>` + 
+        `<button type = "button" onclick = "showupdate(${t.authorID})">Update</button>`
             + "</td></tr> ";
         console.log(t.authorName);
     });
@@ -44,6 +82,11 @@ function remove(id) {
             console.error('Error:', error);
         })
 }
+function showupdate(id) {
+    document.getElementById('authornametoupdate').value = authors.find(t => t['authorID'] == id)['authorName'];
+    document.getElementById('updateformdiv').style.display = 'flex';
+    authorIDtoUpdate = id;
+}
 
 function create() {
     let name = document.getElementById('authorname').value;
@@ -63,6 +106,33 @@ function create() {
             console.log('Success:', data);
                         getdata();
 /*            display();*/
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        })
+
+
+}
+
+function update() {
+    document.getElementById('updateformdiv').style.dislay = 'none';
+    let name = document.getElementById('authornametoupdate').value;
+    fetch('http://localhost:20300/author', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+            {
+                authorName: name,
+                authorID: authorIDtoUpdate
+            }),
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+
         })
         .catch((error) => {
             console.error('Error:', error);

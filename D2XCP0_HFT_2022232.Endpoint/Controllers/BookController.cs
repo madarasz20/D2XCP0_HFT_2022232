dba9241c4205d3d67525185e8c0ea4d66f8d951d
madarasz.oli <madaraszoliver@gmail.com>
2024-05-05 21:59:04 +0200

    wpf signalR

--- D2XCP0_HFT_2022232.Endpoint/Controllers/BookController.cs
+++ D2XCP0_HFT_2022232.Endpoint/Controllers/BookController.cs
@@ -1,6 +1,9 @@
-﻿using D2XCP0_HFT_2022232.Logic;
+﻿using D2XCP0_HFT_2022232.Endpoint.Services;
+using D2XCP0_HFT_2022232.Logic;
 using D2XCP0_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,10 +15,12 @@ namespace D2XCP0_HFT_2022232.Endpoint.Controllers
     public class BookController : ControllerBase
     {
         IBookLogic logic;
+        IHubContext<SignalRHub> hub;
 
-        public BookController(IBookLogic logic)
+        public BookController(IBookLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -34,18 +39,22 @@ namespace D2XCP0_HFT_2022232.Endpoint.Controllers
         public void Create([FromBody] Book value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("BookCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Book value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("BookUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var bookToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("BookDeleted", bookToDelete);
         }
     }
 }
--- D2XCP0_HFT_2022232.Endpoint/Controllers/GenreController.cs
+++ D2XCP0_HFT_2022232.Endpoint/Controllers/GenreController.cs
@@ -1,6 +1,8 @@
-﻿using D2XCP0_HFT_2022232.Logic;
+﻿using D2XCP0_HFT_2022232.Endpoint.Services;
+using D2XCP0_HFT_2022232.Logic;
 using D2XCP0_HFT_2022232.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,10 +14,11 @@ namespace D2XCP0_HFT_2022232.Endpoint.Controllers
     public class GenreController : ControllerBase
     {
         IGenreLogic logic;
-
-        public GenreController(IGenreLogic logic)
+        IHubContext<SignalRHub> hub;
+        public GenreController(IGenreLogic logic, IHubContext<SignalRHub> hub)
         {
             this.logic = logic;
+            this.hub = hub;
         }
 
         [HttpGet]
@@ -34,19 +37,23 @@ namespace D2XCP0_HFT_2022232.Endpoint.Controllers
         public void Create([FromBody] Genre value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("GenreCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Genre value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("GenreUpdated", value);
         }
 
         // DELETE api/<GenreController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var genreToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("GenreDeleted", genreToDelete);
         }
     }
 }
